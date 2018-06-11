74f770304a9b4a7c8edd05e885139ec50d4f9344
Chris Elliott <ctelliott@gmail.com>
2018-06-11 14:25:43 -0400

    Fixed formatting

--- docker/api/src/server.js
+++ docker/api/src/server.js
@@ -1,40 +1,40 @@
 #!/usr/bin/env node
 
 const express = require('express');
-const	basicAuth = require('basic-auth');
-const	logger = require('morgan');
+const basicAuth = require('basic-auth');
+const logger = require('morgan');
 const data = require('./data');
 
-const	api_port = process.env['API_PORT'] || 3000;
+const api_port = process.env['API_PORT'] || 3000;
 const api_host = process.env['API_HOST'] || '0.0.0.0';
-const	api_user = process.env['API_USER'] || 'admin';
-const	api_pass = process.env['API_PASS'];
+const api_user = process.env['API_USER'] || 'admin';
+const api_pass = process.env['API_PASS'];
 
 const app = express();
 app.use(logger('dev'));
 
 var auth = function (req, res, next) {
-	function unauthorized(res) {
-		res.set('WWW-Authenticate', 'Basic realm=Authorization Required');
-		return res.status(401).send({ msg: 'Not Authorized' })
-	}
-	var user = basicAuth(req)
-	if (!user || !user.name || !user.pass) {
-		return unauthorized(res);
-	}
-	if (user.pass === api_pass) {
-		return next();
-	} else {
-		return unauthorized(res);
-	}
+  function unauthorized(res) {
+    res.set('WWW-Authenticate', 'Basic realm=Authorization Required');
+    return res.status(401).send({ msg: 'Not Authorized' })
+  }
+  var user = basicAuth(req)
+  if (!user || !user.name || !user.pass) {
+    return unauthorized(res);
+  }
+  if (user.pass === api_pass) {
+    return next();
+  } else {
+    return unauthorized(res);
+  }
 }
 
 app.get('/healthz', function(req, res, next) {
-	res.status(200).send({status: "ok"});
+  res.status(200).send({status: "ok"});
 })
 
 app.get('/', auth, function(req, res, next) {
-	res.status(200).send(data);
+  res.status(200).send(data);
 })
 
 app.listen(api_port, api_host, function(){
--- docker/frontend/src/server.py
+++ docker/frontend/src/server.py
@@ -11,13 +11,13 @@ options = options.get()
 
 @app.route("/", methods=['GET'])
 def index():
-	url = "http://{api_host}:{api_port}/".format(**options)
-	data = requests.get(url, auth=(options['api_user'], options['api_pass'])).json()
-	if data:
-		data['secret'] = options['secret']
-		return render_template('index', **data)
-	else:
-		return "<!> Coming Soon... </!>"
+    url = "http://{api_host}:{api_port}/".format(**options)
+    data = requests.get(url, auth=(options['api_user'], options['api_pass'])).json()
+    if data:
+        data['secret'] = options['secret']
+        return render_template('index', **data)
+    else:
+        return "<!> Coming Soon... </!>"
 
 if __name__ == "__main__":
-	app.run(host=options['app_host'], port=options['app_port'])
+    app.run(host=options['app_host'], port=options['app_port'])
